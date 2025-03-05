84c8c3bf65b66a1e05f254f4c374e614e87334cc
IrinkaCode <pil02022012@gmail.com>
2025-03-05 15:15:31 +0200

    03.03

--- BackEnd/Model/Client.cs
+++ BackEnd/Model/Client.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BackEnd
+namespace BackEnd.Model
 {
     public partial class Client
     {
--- BackEnd/Model/PostgresContext.cs
+++ BackEnd/Model/PostgresContext.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 
-namespace BackEnd;
+namespace BackEnd.Model;
 
 public partial class PostgresContext : DbContext
 {
